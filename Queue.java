7fa8bb4b1fc0e7e4ed9f9d2af230c74f270ade43
U-CORP\akhandaker <akhandaker@WIN-UI1KPCEMESK.corp.amazonworkspaces.com>
2017-04-18 12:05:57 -0400

    Queue push/put method done.

--- Queue.java
+++ Queue.java
@@ -1,8 +1,33 @@
 class Queue {
+	private int arr[];
+	private int x, y;
+	public int length;
 
+	//Constructor
+	Queue(int size) {
+		arr = new int[size];
+		int x = y = 0;
+		length = size;
+	}
+
+	//put/push integer into the queue
+	public boolean push(int i) {
+		if (x < arr.length-1) {
+			arr[x++] = i;
+			System.out.println("Queue has space");
+			return true;
+		}
+		System.out.println("Queue is full");
+		return false;
+	} 
 }
-class QDemo {
+
+class Qdemo {
 	public static void main(String[] args) {
-		System.out.println("it works");
+		Queue qu = new Queue(15);
+
+		for (int i=0; i<qu.length; i++) {
+			qu.push(i);
+		}
 	}
 }
